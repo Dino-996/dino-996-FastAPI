dca9817fa434be00630bc472267bf9d5df9898c8
dino-996 <davidesabia22@gmail.com>
2026-04-28 01:31:11 +0200

    fix: initialize settings attributes with default values

--- app/core/config.py
+++ app/core/config.py
@@ -2,17 +2,17 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     """ Automatically read variables from the .env file """
-    app_name: str
-    description: str
-    version: str
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
-    refresh_token_expire_days: int
-    database_password: str
-    database_url: str
+    app_name: str = ""
+    description: str = ""
+    version: str = ""
+    secret_key: str = ""
+    algorithm: str = ""
+    access_token_expire_minutes: int = 0
+    refresh_token_expire_days: int = 0
+    database_password: str = ""
+    database_url: str = ""
 
     model_config = SettingsConfigDict(env_file=".env")
 
 # Singleton
-settings = Settings() #type: ignore
+settings = Settings()
