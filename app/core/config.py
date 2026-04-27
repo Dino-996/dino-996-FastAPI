dc032d6d9b58902869a8733483f508d24f47b5e0
dino-996 <davidesabia22@gmail.com>
2026-04-28 01:39:34 +0200

    fix: update settings attributes to use Optional types for better clarity

--- app/core/config.py
+++ app/core/config.py
@@ -2,15 +2,15 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     """ Automatically read variables from the .env file """
-    app_name: str = ""
-    description: str = ""
-    version: str = ""
-    secret_key: str = ""
-    algorithm: str = ""
-    access_token_expire_minutes: int = 0
-    refresh_token_expire_days: int = 0
-    database_password: str = ""
-    database_url: str = ""
+    app_name: str | None = None
+    description: str | None = None
+    version: str | None = None
+    secret_key: str | None = None
+    algorithm: str | None = None
+    access_token_expire_minutes: int | None = None
+    refresh_token_expire_days: int | None = None
+    database_password: str | None = None
+    database_url: str | None = None
 
     model_config = SettingsConfigDict(env_file=".env")
 
